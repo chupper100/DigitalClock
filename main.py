57766d70da1a7bd283c6761a9884e881dc6a9eca
chupper100 <phamminhdat223@gmail.com>
2021-05-11 17:06:22 +0700

    Changed day type

--- main.py
+++ main.py
@@ -2,8 +2,16 @@ from tkinter import*
 from tkinter.ttk import*
 from time import strftime
 
+daydata={
+	'Mon': 'Monday',
+	'Tue': 'Tuesday',
+	'Wed': 'Wednesday'
+}
+formattedday = daydata[strftime('%a')]
+print(formattedday)
+
 def clock():
-    timestring=strftime('%H:%M:%S%p\nDay: %a')
+    timestring=strftime(f'%H:%M:%S%p\nDay: {formattedday}')
     label.config(text=timestring)
     label.after(1000,clock)
 
