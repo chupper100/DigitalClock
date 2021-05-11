5114eb60d3ddbe77bb2d9b2eda119d31c2752a60
chupper100 <phamminhdat223@gmail.com>
2021-05-11 16:51:44 +0700

    Clean code

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 from tkinter import*
 from tkinter.ttk import*
 from time import strftime
-root=Tk()
-root.title("Digital Clock")
 
 def clock():
-	timestring=strftime('%H:%M:%S%p\nDay: %a')
-	label.config(text=timestring)
-	label.after(1000,clock)
+    timestring=strftime('%H:%M:%S%p\nDay: %a')
+    label.config(text=timestring)
+    label.after(1000,clock)
+
+root=Tk()
+root.title("Digital Clock")
 
 label=Label(root, font=("Digital-7",100), background = input('Background:'), foreground = input('Foreground:'))
 label.pack(anchor='center')
